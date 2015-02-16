76466588e290e76b81f652c438910092c80e5fda
Kai Mueller <kaimueller@uni-muenster.de>
2015-02-16 23:37:10 +0100

    fiexed _uv19_uv20_ stuff

--- app/models/primer_read.rb
+++ app/models/primer_read.rb
@@ -106,7 +106,7 @@ class PrimerRead < ActiveRecord::Base
 
       #logic if T7promoter or M13R-pUC.scf:
 
-      if prn == 'T7promoter' #T7 immer forward
+      if prn == 'T7promoter' #T7 is always forward
         # get first part out of m[2]
         rgx = /^_([A-Za-z0-9]+)_([A-Za-z0-9]+)$/
         mtchs= m[2].match(rgx) #--> uv2
