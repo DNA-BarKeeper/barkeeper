f2b97fefdcd9a765d296defd2a49e044f3066d15
Kai Mueller <kaimueller@uni-muenster.de>
2015-07-04 21:57:07 +0200

    added XmlUploader class to auto-generate ABCD representation of db content and make available as S3-URL

---
+++ app/models/xml_uploader.rb
@@ -0,0 +1,38 @@
+class XmlUploader < ActiveRecord::Base
+
+  #todo later rename  :uploaded_file to xml_File or s.th.
+
+  has_attached_file :uploaded_file,
+                    :storage => :s3,
+                    :s3_credentials => Proc.new{ |a| a.instance.s3_credentials },
+                    :default_url => "/specimens.xml"
+
+  # Validate content type
+  validates_attachment_content_type :uploaded_file, :content_type => /\Atext\/plain/
+
+  # Validate filename
+  validates_attachment_file_name :uploaded_file, :matches => [/txt\Z/]
+
+  def create_uploaded_file
+
+    # xml = ::Builder::XmlMarkup.new( :indent => 2 )
+    # xml.instruct! :xml, :encoding => "ASCII"
+    # xml.product do |p|
+    #   p.name "Test"
+    # end
+
+    file_to_upload = File.open("some-file-name.txt", "w")
+    file_to_upload.write("Hurz")
+    file_to_upload.close()
+    self.uploaded_file = File.open("some-file-name.txt")
+    self.save!
+  end
+
+  #todo remove s3 credentials from code everywhere
+
+  def s3_credentials
+    {:bucket => "gbol5", :access_key_id => "AKIAINH5TDSKSWQ6J62A", :secret_access_key => "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"}
+  end
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308202429) do
+ActiveRecord::Schema.define(version: 20150704192029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -370,5 +370,14 @@ ActiveRecord::Schema.define(version: 20150308202429) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "xml_uploaders", force: :cascade do |t|
+    t.string   "uploaded_file_file_name"
+    t.string   "uploaded_file_content_type"
+    t.integer  "uploaded_file_file_size"
+    t.datetime "uploaded_file_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
