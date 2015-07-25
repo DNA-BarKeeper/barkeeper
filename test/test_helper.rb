0b9bcfae3eb9148f23558ce2a53fa4da88d610ad
Kai Mueller <kaimueller@uni-muenster.de>
2015-07-25 11:59:00 +0200

    changed generators to minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/rails'
+require 'minitest/rails/capybara'
 require 'minitest/autorun'
 
 class ActiveSupport::TestCase
