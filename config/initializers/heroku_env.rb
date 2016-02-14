331df77fc46fb2fba93c5a348cb57e22f3f818fe
Kai Mueller <kaimueller@uni-muenster.de>
2016-02-14 16:16:07 +0100

    show changes & link to commit messages

---
+++ config/initializers/heroku_env.rb
@@ -0,0 +1,15 @@
+
+if ENV['HEROKU_RELEASE_VERSION']
+  gbol5_app_version = ENV['HEROKU_RELEASE_VERSION']
+  gbol5_app_version = gbol5_app_version.match(/^v(\d+)/)[1]
+else
+  gbol5_app_version = ''
+end
+
+
+if ENV['HEROKU_RELEASE_CREATED_AT']
+  gbol5_app_release_date = ENV['HEROKU_RELEASE_CREATED_AT']
+  gbol5_app_release_date=Time.zone.parse(gbol5_app_release_date)
+else
+  gbol5_app_release_date = ''
+end
