e51bd8073b275d555632fafc4b680346ae19eec1
Kai Mueller <kaimueller@uni-muenster.de>
2016-05-05 18:03:03 +0200

    text upload

---
+++ app/models/txt_uploader.rb
@@ -0,0 +1,31 @@
+class TxtUploader < ActiveRecord::Base
+
+  has_attached_file :uploaded_file,
+                    :storage => :s3,
+                    :s3_credentials => Proc.new{ |a| a.instance.s3_credentials },
+                    :path => "/output.txt"
+
+  # Validate content type
+  validates_attachment_content_type :uploaded_file, :content_type => /\Aapplication\/txt/
+
+  # Validate filename
+  validates_attachment_file_name :uploaded_file, :matches => [/txt\Z/]
+
+  def create_uploaded_file(text)
+
+    file_to_upload = File.open("output.txt", "w")
+
+    file_to_upload.write(text)
+    file_to_upload.close()
+    self.uploaded_file = File.open("output.txt")
+    self.save!
+
+  end
+
+  #todo remove s3 credentials from code everywhere
+
+  def s3_credentials
+    {:bucket => "gbol5", :access_key_id => "AKIAINH5TDSKSWQ6J62A", :secret_access_key => "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"}
+  end
+
+end
---
+++ test/models/txt_uploader_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class TxtUploaderTest < ActiveSupport::TestCase
+
+  def txt_uploader
+    @txt_uploader ||= TxtUploader.new
+  end
+
+  def test_valid
+    assert txt_uploader.valid?
+  end
+
+end
