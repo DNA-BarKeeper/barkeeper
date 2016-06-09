5ef0fceefcabd4bf7c13ce5b87dc2cc65c67cd31
Kai Mueller <kaimueller@uni-muenster.de>
2016-06-09 15:34:44 +0200

    Division, subdivision, class

---
+++ db/migrate/20160609133011_add_position_to_subdivisions.rb
@@ -0,0 +1,7 @@
+class AddPositionToSubdivisions < ActiveRecord::Migration
+  def change
+    add_column :subdivisions, :position, :integer
+    add_column :subdivisions, :german_name, :string
+    add_column :taxonomic_classes, :german_name, :string
+  end
+end
