80e5a7c7ef0bd396b42f010977837586d919cfe6
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2017-06-21 15:57:58 +0200

    Changes to Capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,7 +56,7 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      unless `git rev-parse HEAD` == `git rev-parse origin/#{fetch(:branch)}`
         puts "WARNING: HEAD is not the same as origin/master"
         puts "Run `git push` to sync changes."
         exit
