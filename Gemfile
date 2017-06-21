471ae0970c2034e90e76563438f5de9a1626c2f3
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2017-06-21 15:54:35 +0200

    Set up capistrano with rbenv

--- Gemfile
+++ Gemfile
@@ -62,7 +62,7 @@ end
 
 group :development do
   gem 'capistrano',         require: false
-  gem 'capistrano-rvm',     require: false
+  gem 'capistrano-rbenv',   require: false
   gem 'capistrano-rails',   require: false
   gem 'capistrano-bundler', require: false
   gem 'capistrano3-puma',   require: false
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,17 @@
 # Change these
 server '46.101.149.34', port: 22, roles: [:web, :app, :db], primary: true
 
-set :repo_url,        'ssh://Sarah_Wiechers@@bitbucket.org/kai42/gbol5.git'
+set :repo_url,        'ssh://Sarah_Wiechers@bitbucket.org/kai42/gbol5.git'
 set :application,     'gbol5'
 set :user,            'sarah'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
+
+# Always deploy currently checked out branch
+set :branch, $1 if `git branch` =~ /\* (\S+)\s/m
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.3'
 
 # Don't change these unless you know what you're doing
 set :pty,             true
@@ -16,9 +22,9 @@ set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(I:/.ssh/id_rsa) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
