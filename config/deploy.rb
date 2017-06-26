2a80b91458449e6dd35ae96121ae3725a44c7c9d
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2017-06-26 15:40:17 +0200

    Changes to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -93,7 +93,7 @@ task :symlink_config_files do
       "#{shared_path}/config/database.yml" => "#{release_path}/config/database.yml",
       "#{shared_path}/config/local_env.yml" => "#{release_path}/config/local_env.yml"
   }
-  run symlinks.map{|from, to| "ln -nfs #{from} #{to}"}.join(" && ")
+  execute symlinks.map{|from, to| "ln -nfs #{from} #{to}"}.join(" && ")
 end
 
 # ps aux | grep puma    # Get puma pid
