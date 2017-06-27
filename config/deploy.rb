6e73eefd16a618019563bb9837b4d00ebc58b07d
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2017-06-27 14:07:53 +0200

    Added sidekiq-log setting to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,6 +29,8 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
+set :sidekiq_log =>   "#{release_path}/log/sidekiq.log"
+
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
