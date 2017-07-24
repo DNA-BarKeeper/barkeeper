8b442f3d38ee0294dcffbb89847e724c326c5a8c
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2017-07-24 13:30:58 +0200

    Changed asset location in nginx.conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  # config.assets.css_compressor = :sass # is automatically used if included in Gemfile and does not need to be set here
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
