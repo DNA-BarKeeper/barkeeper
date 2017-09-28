2e267c02deab7ec6a99571df0607f937bc5efe1a
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2017-09-28 16:24:01 +0200

    Adjusted debugging code in contig.rb

--- app/models/contig.rb
+++ app/models/contig.rb
@@ -567,8 +567,8 @@ class Contig < ActiveRecord::Base
     (1...rows).each { |i|
       (1...cols).each { |j|
         if (s[(read[i-1] + growing_consensus[j-1]).upcase]).nil?
-          STDERR.puts "Error"
-          STDERR.puts read[i-1]
+          Rails.logger.info "Error"
+          Rails.logger.info read[i-1]
         end
         choice1 = a[i-1][j-1] + s[(read[i-1] + growing_consensus[j-1]).upcase] #match
         choice2 = a[i-1][j] + gap #insert
