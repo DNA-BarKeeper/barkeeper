2ec6607c63c75ff60e8341caf1ae96a8ff9c1b32
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2017-10-09 21:13:59 +0200

    Sunburst diagram is now actually used.

--- app/assets/javascripts/sunburst_diagram.js
+++ app/assets/javascripts/sunburst_diagram.js
@@ -12,42 +12,59 @@ $.ajax({
 });
 
 function draw(data) {
-    var color = d3.scaleOrdinal(d3.schemeCategory20b);;
-    var width = 420,
-        barHeight = 20;
+    var width = 960,
+        height = 700,
+        radius = (Math.min(width, height) / 2) - 10;
+
+    var formatNumber = d3.format(",d");
 
     var x = d3.scaleLinear()
-        .range([0, width])
-        .domain([0, d3.max(data)]);
+        .range([0, 2 * Math.PI]);
 
-    var chart = d3.select("#graph")
+    var y = d3.scaleSqrt()
+        .range([0, radius]);
+
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+    var partition = d3.partition();
+
+    var arc = d3.arc()
+        .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x0))); })
+        .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x1))); })
+        .innerRadius(function(d) { return Math.max(0, y(d.y0)); })
+        .outerRadius(function(d) { return Math.max(0, y(d.y1)); });
+
+    var svg = d3.select("body").append("svg")
         .attr("width", width)
-        .attr("height", barHeight * data.length);
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
 
-    var bar = chart.selectAll("g")
-        .data(data)
-        .enter().append("g")
-        .attr("transform", function (d, i) {
-            return "translate(0," + i * barHeight + ")";
-        });
+    data = d3.hierarchy(data);
 
-    bar.append("rect")
-        .attr("width", x)
-        .attr("height", barHeight - 1)
-        .style("fill", function (d) {
-            return color(d)
+    data.sum(function(d) { return d.size; });
+
+    svg.selectAll("path")
+        .data(partition(data).descendants())
+        .enter().append("path")
+        .attr("d", arc)
+        .style("fill", function(d) { return color((d.children ? d : d.parent).data.name); })
+        .on("click", function(d) {
+            svg.transition()
+                .duration(750)
+                .tween("scale", function() {
+                    var xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
+                        yd = d3.interpolate(y.domain(), [d.y0, 1]),
+                        yr = d3.interpolate(y.range(), [d.y0 ? 20 : 0, radius]);
+                    return function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
+                })
+                .selectAll("path")
+                .attrTween("d", function(d) { return function() { return arc(d); }; });
         })
+        .append("title")
+        .text(function(d) { return d.data.name + "\n" + formatNumber(d.value); });
 
-    bar.append("text")
-        .attr("x", function (d) {
-            return x(d) - 10;
-        })
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")
-        .style("fill", "white")
-        .text(function (d) {
-            return d;
-        });
+    d3.select(self.frameElement).style("height", height + "px");
 }
 
 function error() {
