6fda0dfeb0b28b0bd9a4a2a9ad6be529d931b14e
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2017-10-20 14:12:41 +0200

    Added search parameters to contig search. Parameters are now shown on result page.

--- app/models/contig_search.rb
+++ app/models/contig_search.rb
@@ -8,21 +8,21 @@ class ContigSearch < ApplicationRecord
     contigs = Contig.order(:name)
     contigs = contigs.where("name ilike ?", "%#{name}%") if name.present?
 
-    contigs = contigs.where(assembled: true) if assembled.present?
-    contigs = contigs.where(assembled: false) if unassembled.present?
+    contigs = contigs.where(assembled: true) if assembled
+    contigs = contigs.where(assembled: false) if unassembled
 
-    contigs = contigs.where(verified: true) if verified.present?
-    contigs = contigs.where(verified: false) if unverified.present?
+    contigs = contigs.where(verified: true) if verified
+    contigs = contigs.where(verified: false) if unverified
 
     contigs = contigs.where(marker_id: marker_id) if marker_id.present?
 
-    contigs = contigs.joins(isolate: :individual).where("individual.specimen_id ilike ?", "%#{specimen}%") if specimen.present?
+    contigs = contigs.joins(isolate: { individual: {species: :family}}).where(families: {order_id: order_id}) if order_id.present?
+
+    contigs = contigs.joins(isolate: { individual: {species: :family}}).where("families.name ilike ?", "%#{family}%") if family.present?
 
     contigs = contigs.joins(isolate: { individual: :species }).where("species.composed_name ilike ?", "%#{species}%") if species.present?
 
-    # contigs = contigs.joins(isolate: { individual: {species: :family}}).where("family.name ilike ?", "%#{family}%") if family.present?
-
-    # contigs = contigs.joins(isolate: { individual: {species: {family: :order}}}).where(order_id: order_id) if order_id.present?
+    contigs = contigs.joins(isolate: :individual).where("individuals.specimen_id ilike ?", "%#{specimen}%") if specimen.present?
 
     contigs = contigs.where("created_at >= ?", min_age) if min_age.present?
     contigs = contigs.where("created_at <= ?", max_age) if max_age.present?
