6adafbdfc368946817fd9710148a17b744a8831f
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2018-04-11 11:27:29 +0200

    Mat view is now used to dispaly finished sequence diagram

--- app/models/overview_all_taxa_matview.rb
+++ app/models/overview_all_taxa_matview.rb
@@ -6,4 +6,40 @@ class OverviewAllTaxaMatview < ApplicationRecord
   def readonly?
     true
   end
+
+  def self.all_taxa_json
+    root = {:name => 'root', 'children' => []}
+    markers = Marker.gbol_marker.select(:id, :name)
+    data = HigherOrderTaxon.includes(orders: [:families])
+    families_mat = OverviewAllTaxaMatview.group(:family).sum(:species_cnt)
+
+    i = 0
+    markers.each do |marker|
+      children = root['children']
+      children[i] = { :name => marker.name, 'children' => [] }
+      j = 0
+      data.each do |taxon|
+        orders = taxon.orders
+        children2 = children[i]['children']
+        children2[j] = { :name => taxon.name, 'children' => [] }
+        k = 0
+        orders.each do |order|
+          families = order.families
+          children3 = children2[j]['children']
+          children3[k] = { :name => order.name, 'children' => [] }
+          l = 0
+          families.each do |family|
+            children4 = children3[k]['children']
+            children4[l] = { name: family.name, size: families_mat[family.name].to_i }
+            l += 1
+          end
+          k += 1
+        end
+        j += 1
+      end
+      i += 1
+    end
+
+    root
+  end
 end
