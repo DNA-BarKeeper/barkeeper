10a4ee21ef8978f6a5f666f6c64fb25145215df4
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2018-05-25 09:52:18 +0200

    Configured app to use SSL and https links whenever possible.

--- app/models/species_xml_uploader.rb
+++ app/models/species_xml_uploader.rb
@@ -44,7 +44,7 @@ class SpeciesXmlUploader < ApplicationRecord
                    'xmlns:o'=>"urn:schemas-microsoft-com:office:office",
                    'xmlns:x'=>"urn:schemas-microsoft-com:office:excel",
                    'xmlns:ss'=>"urn:schemas-microsoft-com:office:spreadsheet",
-                   'xmlns:html'=>"http://www.w3.org/TR/REC-html40") {
+                   'xmlns:html'=>"https://www.w3.org/TR/REC-html40") {
         xml.Worksheet('ss:Name'=>"Sheet1") {
 
           xml.Table {
