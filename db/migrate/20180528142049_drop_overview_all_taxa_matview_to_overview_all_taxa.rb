5b17aeae29dd0bc59e481c3e8ff0f54e59d06391
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2018-06-04 16:01:01 +0200

    Added 'scenic' gem.

--- db/migrate/20180528142049_drop_overview_all_taxa_matview_to_overview_all_taxa.rb
+++ db/migrate/20180528142049_drop_overview_all_taxa_matview_to_overview_all_taxa.rb
@@ -1,4 +1,4 @@
-class RenameOverviewAllTaxaMatviewToOverviewAllTaxa < ActiveRecord::Migration[5.0]
+class DropOverviewAllTaxaMatviewToOverviewAllTaxa < ActiveRecord::Migration[5.0]
   def change
     drop_view :overview_all_taxa_matviews, materialized: true
   end
