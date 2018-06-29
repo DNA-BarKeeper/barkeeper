8d51727689dc31828c78f12b7e746e8efa979428
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2018-06-29 13:26:36 +0200

    Added more detailed output and ability to do alignment on server to ssh.rake.

--- app/models/mislabel_analysis.rb
+++ app/models/mislabel_analysis.rb
@@ -4,13 +4,13 @@ class MislabelAnalysis < ApplicationRecord
   has_and_belongs_to_many :marker_sequences
 
   def percentage_of_mislabels
-    ((mislabels.size / marker_sequences.size) * 100).round(2)
+    ((mislabels.size / total_seq_number.to_f) * 100).round(2) if total_seq_number&.positive?
   end
 
-  def self.import(file, title, marker_id = nil, automatic = false)
+  def self.import(file, title, total_seq_number = 0, marker_id = nil, automatic = false)
     column_names = %w[SeqID MislabeledLevel OriginalLabel ProposedLabel Confidence OriginalTaxonomyPath ProposedTaxonomyPath PerRankConfidence]
 
-    mislabel_analysis = MislabelAnalysis.create(title: title, marker_id: marker_id, automatic: automatic)
+    mislabel_analysis = MislabelAnalysis.create(title: title, marker_id: marker_id, automatic: automatic, total_seq_number: total_seq_number)
 
     # Parse file
     File.open(file, 'r').each do |row|
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180628115013) do
+ActiveRecord::Schema.define(version: 20180629094941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -364,10 +364,11 @@ ActiveRecord::Schema.define(version: 20180628115013) do
 
   create_table "mislabel_analyses", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.boolean  "automatic",  default: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.boolean  "automatic",        default: false
     t.integer  "marker_id"
+    t.integer  "total_seq_number"
     t.index ["marker_id"], name: "index_mislabel_analyses_on_marker_id", using: :btree
   end
 
