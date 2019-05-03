2471ff827ff4324242eb92f0e22b61c239feb7fb
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2019-05-03 16:36:18 +0200

    Added values to seed.rb

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 class CreateAdminService
-  def call
+  def call(projects)
     user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
       user.password = Rails.application.secrets.admin_password
       user.password_confirmation = Rails.application.secrets.admin_password
+      user.name = Rails.application.secrets.admin_name
+      user.role = 'admin'
+      user.projects = projects
     end
   end
 end
