6e5425b68bb2be863fb56b89b6832da9c7862ea9
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2019-09-17 10:51:05 +0200

    Added Herbarium Scaffold.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190913163724) do
+ActiveRecord::Schema.define(version: 20190916150552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -178,6 +178,13 @@ ActiveRecord::Schema.define(version: 20190913163724) do
   create_table "helps", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "herbaria", force: :cascade do |t|
+    t.string   "name"
+    t.string   "acronym"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "higher_order_taxa", force: :cascade do |t|
