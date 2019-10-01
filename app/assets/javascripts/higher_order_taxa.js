b77dfcde37b97e79e265c2320ceda158d1d85c3e
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2019-10-01 15:19:29 +0200

    Higher Order Taxon index view now dispalys tree structure.

--- app/assets/javascripts/higher_order_taxa.js
+++ app/assets/javascripts/higher_order_taxa.js
@@ -1,14 +1,15 @@
 jQuery(function() {
-    $('#higher_order_taxa').DataTable({
-        "columnDefs": [
-            {"orderable": false, "targets": 3}
-        ],
-        "order": [ 2, 'asc' ]
-    });
-
     $('#higher_order_taxon_marker_ids').chosen({
         allow_single_deselect: true,
-        no_results_text: 'No results matched',
-        width: '400px'
+        no_results_text: 'No results matched'
+    });
+
+    // $('#higher_order_taxon_project_ids').chosen({
+    //     allow_single_deselect: true,
+    //     no_results_text: 'No results matched'
+    // });
+
+    return $('#higher_order_taxon_parent_id').autocomplete({
+        source: $('#higher_order_taxon_parent_id').data('autocomplete-source')
     });
 });
