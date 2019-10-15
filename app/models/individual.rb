e604ffe92cf2b7c3c9f833530d8e66373fdbe78b
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2019-10-15 16:03:13 +0200

    Specimen with negative control isolates are not shown in specimen without species view anymore.

--- app/models/individual.rb
+++ app/models/individual.rb
@@ -14,7 +14,7 @@ class Individual < ApplicationRecord
 
   pg_search_scope :quick_search, against: %i[specimen_id herbarium collector collectors_field_number]
 
-  scope :without_species, -> { where(species: nil) }
+  scope :without_species, -> { joins(:isolates).where.not(isolates: { negative_control: true }).where(species: nil) }
   scope :without_isolates, -> { left_outer_joins(:isolates).select(:id).group(:id).having('count(isolates.id) = 0') }
   scope :no_species_isolates, -> { without_species.left_outer_joins(:isolates).select(:id).group(:id).having('count(isolates.id) = 0') }
   scope :bad_longitude, -> { where(longitude: nil).where.not(longitude_original: [nil, ''])
