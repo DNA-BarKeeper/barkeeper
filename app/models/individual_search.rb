7609daa638e7480fc00d679140742ff266e257d4
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2020-08-07 13:47:43 +0200

    Search title is now only validated for uniqueness per user

--- app/models/individual_search.rb
+++ app/models/individual_search.rb
@@ -4,7 +4,7 @@ class IndividualSearch < ApplicationRecord
   belongs_to :user
   belongs_to :project
 
-  validates_uniqueness_of :title
+  validates :title, uniqueness: { :scope=> :user_id }
 
   enum has_issue: %i[all_issue issues no_issues]
   enum has_species: %i[all_species species no_species]
