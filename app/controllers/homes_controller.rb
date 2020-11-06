4d2532cfa89ffe1296bccb6d04b17d8148a890f7
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2020-11-06 14:56:35 +0100

    Initial try to inplement project logo upload

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -60,7 +60,8 @@ class HomesController < ApplicationController
   end
 
   def home_params
-    params.require(:home).permit(:active, :description, :subtitle, :title, :background_image,
-                                 :delete_background_image, logos_attributes: [:id, :title, :url, :image, :delete_image])
+    params.require(:home).permit(:active, :description, :subtitle, :title, :background_image, :delete_background_image,
+                                 project_logo_attributes: [ :id, :url, :image, :_destroy ],
+                                 logos_attributes: [:id, :title, :url, :image, :delete_image, :_destroy])
   end
 end
--- app/models/home.rb
+++ app/models/home.rb
@@ -1,14 +1,17 @@
 class Home < ApplicationRecord
-  has_many :logos
+  has_one :project_logo, class_name: 'Logo', dependent: :destroy
+  has_many :logos, dependent: :destroy
 
   has_one_attached :background_image
   validates :background_image, content_type: [:jpg, :png, :svg]
 
   validates_presence_of :title
+  validates_length_of :logos, :maximum => 12
 
   attr_accessor :delete_background_image
   before_validation :remove_background_image
 
+  accepts_nested_attributes_for :project_logo, allow_destroy: true
   accepts_nested_attributes_for :logos, allow_destroy: true
 
   def remove_background_image
