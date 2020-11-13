cf9783c1f3f27ef0c91a81f5f28eccb824f3611f
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2020-11-13 15:44:40 +0100

    Checked licenses of dependancies for compatibility. Added License notice to overview_diagram.js.

--- app/assets/javascripts/overview_diagram.js
+++ app/assets/javascripts/overview_diagram.js
@@ -1,24 +1,24 @@
 /*
- * Barcode Workflow Manager - A web framework to assemble, analyze and manage DNA
- * barcode data and metadata.
- * Copyright (C) 2020 Kai Müller <kaimueller@uni-muenster.de>, Sarah Wiechers
- * <sarah.wiechers@uni-muenster.de>
- *
- * This file is part of Barcode Workflow Manager.
- *
- * Barcode Workflow Manager is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * Barcode Workflow Manager is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Barcode Workflow Manager.  If not, see
- * <http://www.gnu.org/licenses/>.
+Copyright 2013 Google Inc. All Rights Reserved.
+
+Modifications copyright (C) 2020 Kai Müller <kaimueller@uni-muenster.de>, Sarah Wiechers
+<sarah.wiechers@uni-muenster.de>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+/*
+Original code for sunburst diagram may be found here: https://bl.ocks.org/kerryrodden/766f8f6d31f645c39f488a0befa1e3c8
  */
 
 jQuery(function() {
