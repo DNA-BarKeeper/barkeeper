b1d9df5f381f9fd1afb6c1df51c862088f5eda29
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2021-05-10 15:51:37 +0200

    Removed remaining mentions of other taxonomic models.

--- spec/factories/contig_searches.rb
+++ spec/factories/contig_searches.rb
@@ -4,7 +4,6 @@ include ActionDispatch::TestProcess
 FactoryBot.define do
   factory :contig_search do |cs|
     cs.assembled { ["assembled", "unassembled", "both"].sample }
-    cs.family { Faker::Lorem.word }
     cs.has_warnings { ContigSearch.has_warnings.keys.sample }
     cs.marker { Faker::Lorem.word }
     cs.max_age { Faker::Time.backward(days: 176) }
@@ -12,8 +11,7 @@ FactoryBot.define do
     cs.min_age { Faker::Time.backward(days: 1) }
     cs.min_update { Faker::Time.backward(days: 1) }
     cs.name { Faker::Lorem.word }
-    cs.order { Faker::Lorem.word }
-    cs.species { Faker::Lorem.word }
+    cs.taxon { Faker::Lorem.word }
     cs.specimen { Faker::Lorem.word }
     cs.title { Faker::Lorem.word }
     cs.verified { ["verified", "unverified", "both"].sample }
