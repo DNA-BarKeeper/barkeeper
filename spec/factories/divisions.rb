28a9a0e6844bfbcb6f616120d8e2c18dfb36b5c8
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2021-05-10 11:23:26 +0200

    Removed specs and factories for old taxonomic models.

--- spec/factories/divisions.rb
+++
@@ -1,7 +0,0 @@
-require 'faker'
-
-FactoryBot.define do
-  factory :division do |d|
-    d.name { Faker::Lorem.word }
-  end
-end
--- spec/factories/subdividions.rb
+++
@@ -1,9 +0,0 @@
-require 'faker'
-
-FactoryBot.define do
-  factory :subdivision do |s|
-    s.german_name { Faker::Lorem.word }
-    s.name { Faker::Lorem.word }
-    s.position { Faker::Number.digit }
-  end
-end
