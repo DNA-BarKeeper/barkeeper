4aaa379ef539695c37ecfa619f8075ed51efd647
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2021-05-10 11:28:57 +0200

    Removed remaining files for old taxonomic models.

--- app/models/division.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Division < ApplicationRecord
-  has_many :subdivisions
-end
--- app/models/subdivision.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class Subdivision < ApplicationRecord
-  has_many :taxonomic_classes
-  belongs_to :division
-end
