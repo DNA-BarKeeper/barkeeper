c0c45584da96cd46dfb8f545d607142e520d993c
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2021-05-12 10:02:15 +0200

    Replaced autocomplete for parent taxon with chosen select and added chosen-bootstrap3 styles.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,7 +7,7 @@
 //= require jquery.autosize
 //= require jquery-fileupload/basic
 //= require jquery-fileupload/vendor/tmpl
-//= require chosen.jquery
+//= require chosen-jquery
 //= require bootstrap
 //= require keyboard_shortcuts
 //= require d3.min
