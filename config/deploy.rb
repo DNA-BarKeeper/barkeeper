0d76c92b1379b5ec97f3f715fe67734c2bd2a4ac
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2021-07-02 14:14:49 +0200

    Changed path to puma log files to sahred location

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,8 +32,8 @@ set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.access.log"
-set :puma_error_log,  "#{release_path}/log/puma.error.log"
+set :puma_access_log, "#{shared_path}/log/puma.access.log"
+set :puma_error_log,  "#{shared_path}/log/puma.error.log"
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true # Change to false when not using ActiveRecord
