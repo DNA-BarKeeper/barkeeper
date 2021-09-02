a48335e617e8f76bcd6e481c001648e1e0a88c87
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2021-09-02 17:34:32 +0200

    Gallery with slideshow and remove buttons inside modal.

--- app/assets/javascripts/individuals.js
+++ app/assets/javascripts/individuals.js
@@ -26,12 +26,20 @@ jQuery(function() {
         dateFormat: 'yy-mm-dd'
     });
 
+    // Slideshow gallery with lightbox
+    function openModal() {
+        document.getElementById("myModal").style.display = "block";
+    }
+
+    function closeModal() {
+        document.getElementById("myModal").style.display = "none";
+    }
+
     // Next/previous controls
     function plusSlides(n) {
         showSlides(slideIndex += n);
     }
 
-    // Thumbnail image controls
     function currentSlide(n) {
         showSlides(slideIndex = n);
     }
@@ -39,33 +47,62 @@ jQuery(function() {
     function showSlides(n) {
         var i;
         var slides = document.getElementsByClassName("mySlides");
+        var modalSlides = document.getElementsByClassName("modalSlides");
+
         var dots = document.getElementsByClassName("dot");
+        var captionText = document.getElementById("caption");
+
         var remove_buttons = document.getElementsByClassName("remove-image");
+        var remove_buttons_modal = document.getElementsByClassName("remove-image-modal");
 
         if (n > slides.length) {slideIndex = 1}
         if (n < 1) {slideIndex = slides.length}
+
         for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";
+            modalSlides[i].style.display = "none";
         }
         for (i = 0; i < dots.length; i++) {
             dots[i].className = dots[i].className.replace(" dot-active", "");
         }
+
         for (i = 0; i < remove_buttons.length; i++) {
             remove_buttons[i].style.display = "none";
+            remove_buttons_modal[i].style.display = "none";
         }
         slides[slideIndex-1].style.display = "block";
+        modalSlides[slideIndex-1].style.display = "block";
+
         remove_buttons[slideIndex-1].style.display = "block";
+        remove_buttons_modal[slideIndex-1].style.display = "block";
+
         dots[slideIndex-1].className += " dot-active";
+        captionText.innerHTML = slides[slideIndex-1].title;
     }
 
-    var slideIndex = 1;
-    showSlides(slideIndex);
+    if (document.getElementById("slideshow-wrapper") != null) {
+        var slideIndex = 1;
+        showSlides(slideIndex);
+    }
 
     $('a.prev').click(function() { plusSlides(-1) });
     $('a.next').click(function() { plusSlides(1) });
 
     $('span.dot').click(function() {
-        var index = this.getAttribute("data-index");
-        currentSlide(index)
+        var index = parseInt(this.getAttribute("data-index"));
+        currentSlide(index);
+    });
+
+    $('img.preview-image').click(function() {
+        openModal();
+        var index = parseInt(this.getAttribute("data-index"));
+        currentSlide(index);
+    });
+
+    $(document).click(function(event) {
+        // If you click on anything except the modal itself, close the modal
+        if (!$(event.target).closest(".modalSlides,.preview-image,.next,.prev").length) {
+            closeModal();
+        }
     });
 });
