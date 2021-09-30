9ff4aed6eb2999e7ae4fa58036106a1fb5687346
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2021-09-30 14:59:06 +0200

    Moved helper method from deprecated concern to ApplicationHelper.

--- app/helpers/overview_diagrams_helper.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module OverviewDiagramsHelper
-  def current_project_id
-    user_signed_in? ? current_user.default_project_id : Project.where('name like ?', 'All%').first.id
-  end
-end
