88cee489a7bd41108aa43ffb1adb4bcac178ccd5
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2021-11-25 10:37:34 +0100

    Added mail service for production.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 } # Necessary for Devise mails
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => 'maildev',
+    :address => 'mail',
     :port => 25,
     :openssl_verify_mode => 'none'
   }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,11 +63,18 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "gbo_lapp_#{Rails.env}"
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = { host: ENV['PROJECT_DOMAIN'] } # Necessary for Devise mails
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'mail',
+    :port => 25
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -91,15 +98,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # For Devise
-  config.action_mailer.default_url_options = { host: ENV['PROJECT_DOMAIN'] }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address: '127.0.0.1',
-      port: 25,
-      domain: ENV['PROJECT_DOMAIN'],
-      tls: false,
-      enable_starttls_auto: false
-  }
 end
