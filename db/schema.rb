62a54a4cee25e3361f824cd86ed4783f6a843632
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2023-02-07 15:02:19 +0100

    Added info to documentation.

--- db/schema.rb
+++ db/schema.rb
@@ -210,6 +210,7 @@ ActiveRecord::Schema.define(version: 2022_09_16_082521) do
     t.text "locality"
     t.string "latitude_original", limit: 255
     t.string "longitude_original", limit: 255
+    t.string "elevation_orig", limit: 255
     t.string "exposition", limit: 255
     t.text "habitat"
     t.string "substrate", limit: 255
@@ -226,7 +227,6 @@ ActiveRecord::Schema.define(version: 2022_09_16_082521) do
     t.integer "collection_id"
     t.integer "tissue_id"
     t.bigint "taxon_id"
-    t.string "elevation_orig"
     t.decimal "elevation"
     t.index ["collection_id"], name: "index_individuals_on_collection_id"
     t.index ["taxon_id"], name: "index_individuals_on_taxon_id"
